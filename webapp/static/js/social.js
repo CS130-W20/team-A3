a86dd3cf575623034ac21c04409dc1092182b66d
song <songjiang@cs.ucla.edu>
2020-02-18 14:15:48 -0800

    resubmit

--- webapp/static/js/social.js
+++
@@ -1,9 +0,0 @@
-function handeling_friend_request() {
-    alert("we know that a friend request is sent to user " + show_user.id + " " + show_user.name + "\n"
-        + "but it is not handled yet");
-}
-
-function user_chat() {
-    alert("we know that you want to chat with user " + show_user.id + " " + show_user.name + "\n"
-        + "but it is not implemented yet");
-}
--- webapp/visualization/controllers.py
+++
@@ -1,7 +0,0 @@
-from visualization.__init__ import *
-
-visualizer = Blueprint('visualizer', __name__)
-
-@visualizer.route('/concepts')
-def showing_concepts():
-    return render_template('visualize.html')
