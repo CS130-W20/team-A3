f25809d653d8a35ee9bca48a2afcbbab0026b3b8
Yugo Watanabe <yugowatanabe@g.ucla.edu>
2020-02-18 21:03:15 -0800

    add users directory skeleton

---
+++ users/__init__.py
@@ -0,0 +1,17 @@
+import json
+import datetime
+import random
+import configparser
+import os
+
+# Import flask dependencies
+from flask import Blueprint, request, render_template, \
+                  flash, g, session, redirect, url_for
+
+# Import the database object from the main app module
+from __init__ import db, login_manager
+
+config = configparser.ConfigParser()
+config.read('config.ini', 'utf-8')
+USERDB_PATH = config['DEFAULT']['user_db_path']
+CONFIG_PATH = 'config.ini'
