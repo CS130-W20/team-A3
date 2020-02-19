eb4aaae0110d802a40025829feb7d7656f0f65b4
Kaiyuan Chen <chenkaiyuan@ucla.edu>
2020-02-19 14:45:00 -0800

    Concept page (#39)
    * add find concept handler
    
    * add concept page rendering
    
    * Error Handling
    
    * Fix bug on import
    
    * Fix paths
    
    * concept template
    
    * wiki crawler
    
    * concept list
    
    * main loop
    
    * wiki_concepts

--- database/data_concepts/wiki.py
+++ database/data_concepts/wiki.py
@@ -1,22 +1,50 @@
 import sys, requests, bs4
 import xml.etree.ElementTree as ET
+import pickle
+
+def get_pickle_file():
+    f = open("combined_concept_list.pkl", 'rb')
+    ret = pickle.load(f)
+    f.close()
+    return ret
 
 def get_wiki(concept):
     res = requests.get("https://en.wikipedia.org/wiki/" + concept)
     res.raise_for_status()
     wiki = bs4.BeautifulSoup(res.text, "html.parser")
+    counter = 0
+    ret_text = ""
+    
     for i in wiki.select("p"):
-        return (i.getText())
+        ret_text += i.getText()
+        counter += 1
+        if counter > 3:
+            return ret_text
+    return ret_text
 
+def get_xml(concept, concept_name_str):
+    # create the file structure
+    data = ET.Element('doc')
+    id = ET.SubElement(data,'id')
+    concept_name = ET.SubElement(data, 'concept_name')
+    wiki = ET.SubElement(data, 'wiki')
+    id.text = concept
+    concept_name.text = concept_name_str
+    wiki.text = get_wiki(concept)
+    with open(concept+".xml", 'wb') as f:
+        f.write(ET.tostring(data))
 
-# create the file structure
-data = ET.Element('doc')
-id = ET.SubElement(data,'id')
-concept_name = ET.SubElement(data, 'concept_name')
-wiki = ET.SubElement(data, 'wiki')
-id.text = "Concept_01"
-concept_name.text = "Machine Learning"
-wiki.text = get_wiki("machine_learning")
+def concept_to_string(c):
+    return c.replace(" ", "_")
 
-with open("Concept_01.xml", 'wb') as f:
-    f.write(ET.tostring(data))
+if __name__ == "__main__":
+    concept_list = get_pickle_file()
+    for concept in concept_list:
+        s = (concept_to_string(concept))
+        print(s)
+        try:
+            get_xml(s, concept)
+        except requests.exceptions.HTTPError:
+            with open("not_found_entries", "a+") as f:
+                f.write(s + "\n")
+                
