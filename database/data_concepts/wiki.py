c73f04ec5fb0346bca6a6a429eb6533e49f9c1a9
Kaiyuan <chenkaiyuan@ucla.edu>
2020-02-19 14:22:43 -0800

    main loop

--- database/data_concepts/wiki.py
+++ database/data_concepts/wiki.py
@@ -1,5 +1,12 @@
 import sys, requests, bs4
 import xml.etree.ElementTree as ET
+import pickle
+
+def get_pickle_file():
+    f = open("combined_concept_list.pkl", 'rb')
+    ret = pickle.load(f)
+    f.close()
+    return ret
 
 def get_wiki(concept):
     res = requests.get("https://en.wikipedia.org/wiki/" + concept)
@@ -8,15 +15,24 @@ def get_wiki(concept):
     for i in wiki.select("p"):
         return (i.getText())
 
+def get_xml(concept):
+    # create the file structure
+    data = ET.Element('doc')
+    id = ET.SubElement(data,'id')
+    concept_name = ET.SubElement(data, 'concept_name')
+    wiki = ET.SubElement(data, 'wiki')
+    id.text = "Concept_01"
+    concept_name.text = "Machine Learning"
+    wiki.text = get_wiki("machine_learning")
+    with open("Concept_01.xml", 'wb') as f:
+        f.write(ET.tostring(data))
 
-# create the file structure
-data = ET.Element('doc')
-id = ET.SubElement(data,'id')
-concept_name = ET.SubElement(data, 'concept_name')
-wiki = ET.SubElement(data, 'wiki')
-id.text = "Concept_01"
-concept_name.text = "Machine Learning"
-wiki.text = get_wiki("machine_learning")
+def concept_to_string(c):
+    return c.replace(" ", "_")
 
-with open("Concept_01.xml", 'wb') as f:
-    f.write(ET.tostring(data))
+if __name__ == "__main__":
+    concept_list = get_pickle_file()
+    for concept in concept_list:
+        s = (concept_to_string(concept))
+        print(s)
+        print(get_wiki(s))
