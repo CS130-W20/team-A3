58103d3a054809347e244ba3a56261eafe8c130a
Zijie <zijiehjj@gmail.com>
2020-02-18 20:49:00 -0800

    Browse Mode Revision (#21)
    * creating browse mode dir and course taxonomy
    
    * Parse original course_taxonomy into structured data
    
    * Basic Functionality in Browse mode, including click box and expand; click box and show corresponding category introduction
    
    * adding browse mode as bluepoint in the init file. Updated the main function.
    
    * Crawler for courses

--- __init__.py
+++ __init__.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template, g, request, session, url_for, redirect
+from flask import Flask, render_template, g, session
 from flask_login import current_user, LoginManager
-import json
-
 # Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
+
+
 # Define the WSGI application object
 application = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(application)
-
-
-
-
 
 
 @application.errorhandler(404)
@@ -26,19 +22,23 @@ application.config.from_object('config')
 # by modules and controllers
 db = SQLAlchemy(application)
 
+#Get course_taxonomy：
+from browse.generate_categories import generate_course_list
+browse_categories = generate_course_list("browse/course_taxonomy.txt")
+
 # Import a module / component using its blueprint handler variable (mod_auth)
 from auth.controllers import auth
 from search.controllers import searcher
 from users.controllers import users
 from visualization.controllers import visualizer
+from browse.controllers import browse,get_category_info
 
 # Register blueprint(s)
 application.register_blueprint(auth)
 application.register_blueprint(searcher)
 application.register_blueprint(users)
 application.register_blueprint(visualizer)
-
-# handeling the category selection
+application.register_blueprint(browse)
 
 
 
@@ -53,6 +53,9 @@ def main():
 
     :return: HTML Template for the landing page.
     """
+    if session.get("current_category") is None:
+        session["current_category"] = get_category_info(-1)
+    return render_template('search.html', nonempty=True, welcome=True, browse_categories=browse_categories)
 
 
 
