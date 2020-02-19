621d1caa149fcd7843fc230519fe7ca2fdee9276
rainyrainyguo <1695428348@qq.com>
2020-02-19 10:23:31 -0800

    add COURSEDB_PATH

--- search/__init__.py
+++ search/__init__.py
@@ -3,4 +3,5 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini', 'utf-8')
 DIR_PATH = config['DEFAULT']['doc_dir_path']
+COURSEDB_PATH = config['DEFAULT']['db_path'] #added 
 CONFIG_PATH = 'config.ini'
