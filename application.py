7812fa35baae18f147953b17ba4d76696c1c6fd4
Kaiyuan Chen <chenkaiyuan@ucla.edu>
2020-03-06 20:52:00 -0800

    fix testing script

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 # Run a test server.
 #from app import application
 from __init__ import application
+import sys 
 if __name__ == '__main__':
+	if len(sys.argv) == 2 and sys.argv[1] == "test":
+		exit(0)
 	application.run(host='0.0.0.0', port=8080, debug=True)
